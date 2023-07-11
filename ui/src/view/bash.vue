b0d2c33a73c9b9bd1a40d2c8e171fc524ebf327b
mizhidajiangyou <670746945@qq.com>
2023-07-11 19:19:11 +0800

    nginx配置

---
+++ ui/src/view/bash.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="text" v-model="content">
+        <button @click="submit">OK</button>
+        <div>{{ result }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            content: '',
+            result: ''
+        }
+    },
+    methods: {
+        async submit() {
+            const response = await this.$http.post('/api/bash-api/normal', { content: this.content });
+            this.result = response.data.result;
+        }
+    }
+}
+</script>
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,3 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:18080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
